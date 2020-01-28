48dc2c1be0406e25badca394824b7df8d0ae65b6
milkteahot <greenegoode11@gmail.com>
2020-01-28 17:21:33 +0000

    add py2

--- programmers/py.java
+++ programmers/py.java
@@ -1,16 +1,20 @@
+import java.util.*;
+import java.io.*;
 class Solution {
-    boolean solution(String s) {
+    boolean solution(String a) {
         boolean answer = true;
-        String[] t1 = s.split("");
+        char[] t1 = a.toCharArray();
         int p=0;
         int y=0;
         for(int i=0;i<t1.length;i++){
-            if(t1[i] == "p" || t1[i] =="P") p++;
-            if(t1[i] == "y" || t1[i] =="Y") y++;
+            if(t1[i] == 'p' || t1[i] =='P') 
+                p++;
+            else if(t1[i] == 'y' || t1[i] =='Y') 
+                y++;
         }
-        if(p!=y) answer = false;
-        else answer = true;
-
+        if(p == y) 
+            answer = true;
+        else answer = false;
         return answer;
     }
 }
