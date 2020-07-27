d6297b5a580fa38b6102e725299fa17725ecd957
milkteahot <greenegoode11@gmail.com>
2020-07-27 14:21:06 +0900

    JS average

---
+++ programmers/javascript/average.js
@@ -0,0 +1,13 @@
+function average(array) {
+    return array.reduce((a,b) => a+b) / array.length;
+}
+
+/*
+function solution(arr) {
+    var answer = 0;
+    for(var i=0;i<arr.length;i++){
+        answer += arr[i];
+    }
+    return answer = answer/arr.length;
+}
+*/
