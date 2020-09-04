7a74be29f34920a691c86f50c49dc67a0f37832a
milkteahot <greenegoode11@gmail.com>
2020-09-04 10:24:22 +0900

    Add _map

--- FunctionalProgramming/_map.js
+++ FunctionalProgramming/_map.js
@@ -9,32 +9,15 @@ var users = [
     {id:8, name: 'MP', age: 23},
 ]
 
-//2. _filter, _map 으로 리팩토링
 function _filter(list, predi) {
     var new_list = [];
     for(var i=0;i<list.length;i++){
         if(predi(list[i])) {
-            new_list.push(list[i]);
+            new_list.push(list[i])
         }
     }
     return new_list;
 }
-
-// console.log(
-//     _filter(users, function(user) {
-//         return user.age >= 30;
-//     })
-// )
-
-// console.log(
-//     _filter(users, function(user) {
-//         return user.age < 30;
-//     })
-// )
-
-// console.log(
-//     _filter([1,2,3,4], function(num){ return num%2; })
-// )
 
 function _map(list, mapper){
     var new_list = [];
@@ -47,4 +30,8 @@ function _map(list, mapper){
 var over_30 = _filter(users, function(user){ 
     return user.age >= 30 
 });
-console.log("over_30", over_30)
+console.log(over_30)
+
+var names = _map(over_30, function(user){ return user.name; })
+
+console.log(names)
