c27f43f433d4f2b86d981d771b0c8f8929dfaafa
milkteahot <greenegoode11@gmail.com>
2020-09-16 12:07:43 +0900

    Add Programmers 큰수만들기(Greedy)

---
+++ programmers/javascript/biggestNumber.js
@@ -0,0 +1,18 @@
+function solution(number, k) {
+    var answer = [];
+    for(var i=0;i<number.length;i++){
+        var now = number[i]; //현재 인덱스값
+        //이전 인덱스값이 현재 인덱스값보다 작으면 제거
+        while(k>0 && answer[answer.length-1] < now) {
+            answer.pop();
+            k--;
+        }
+        //현재 인덱스값 추가
+        answer.push(now);
+    }
+    //k개수가 남은 경우 처리
+    answer.splice(answer.length-k,k);
+    answer = answer.join('');
+    return answer;
+    
+}
