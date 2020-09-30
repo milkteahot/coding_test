41e102e653af1b22b8226e20dd456a578db4d80d
milkteahot <greenegoode11@gmail.com>
2020-09-30 22:09:46 +0900

    Add find PrimeNumber

--- programmers/javascript/findPrimeNumber.js
+++ programmers/javascript/findPrimeNumber.js
@@ -1,15 +1,32 @@
 function solution(numbers) {
     var answer  = '';
-    var n = numbers.split('').sort((a,b)=>b-a)
-    var cnt = 0;
-    for(var a=2;a<=n.length;a++){
-         cnt=0;
-        for(var b=1;b<=a;b++){
-            if(a%b==0)
-            cnt++;
+    var primeNumbers = [];  
+    var n = numbers.split('').sort((a,b)=>b-a);
+    
+    const isPrime = (num) => {
+        let count = 0;
+        for (let i = 1; i <= num; i++) {
+            if (num % i === 0) {
+                count++;
+            }
+            if (count >= 3) {
+                break;
+            }
         }
-        if(cnt==2)
-        answer++;
+        if (count === 2 && !primeNumbers.includes(num)) {
+            primeNumbers.push(num);
+        }
     }
-    return answer;
-}
+   const mergeNumbers = (arr, str) => {
+    if (arr.length > 0) {
+        for (let i = 0; i < arr.length; i++) {
+            const temp = [...arr];
+            temp.splice(i, 1);
+            mergeNumbers(temp, str + arr[i]);
+        }
+    }
+    if (str.length > 0) isPrime(+str);
+    };
+    mergeNumbers(n, "");
+    return answer = primeNumbers.length;
+    };
