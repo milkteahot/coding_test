63ca6fa8ac219fddd3e4bc89f3a56feca63cbc16
milkteahot <greenegoode11@gmail.com>
2020-10-14 15:53:17 +0900

    biggestNum, h-index review

--- programmers/javascript/biggestNumber.js
+++ programmers/javascript/biggestNumber.js
@@ -1,6 +1,6 @@
 //Greedy
-var number = "1924"
-var k = 2;
+// var number = "1924"
+// var k = 2;
 
 function solution(number, k) {
     var answer = [];
@@ -23,5 +23,19 @@ function solution(number, k) {
 
 //리뷰 10.07
 function solution(number, k){
-    
+    var answer = [];
+    //포문으로 number길이만큼 반복하면서 현재값과 이전값 비교
+    for(var i=0;i<number.length;i++){
+        var now = number[i];
+        //k횟수가 남아있고, 이전값이 현재값보다 작으면 => 이전값을 answer스택에서 제거
+        while(k>0 && answer[answer.length-1] < now) {
+            answer.pop()
+            k--;
+        }
+        //answer스택에 현재 인덱스값 넣기
+        answer.push(now);
+    }
+    //k횟수가 남은 경우 예)9999,2 -> answer길이-k 인덱스부터 k수만큼 제거
+    answer.splice(answer.length - k, k);
+    return answer = answer.join('');
 }
--- programmers/javascript/h-index.js
+++ programmers/javascript/h-index.js
@@ -10,3 +10,45 @@ function solution(citations) {
     }
     return answer-1;
 }
+
+//10.14리뷰
+function solution(citations) {
+    var answer = 0;
+    //인용횟수배열을 오름차순 정렬. a-b 음수면 a<b,0이면 같다
+    var temp = citations.sort((a,b)=>a-b);
+    while(true){
+        //인덱스값이 answer보다 크거나 같으면 answer+1
+        var temp2 = temp.filter(v=>v>=answer).length
+        if(temp2<answer){
+            break;
+        }
+        answer++;
+
+    }
+
+    return answer;
+}
+
+//10.14리뷰(2)
+//예) [1,7,0,1,6,4]
+function solution(citations) {
+    var answer = 0;
+    //인용횟수배열을 내림차순 정렬. a-b 음수면 a<b,0이면 같다
+    //[7,6,4,1,1,0]
+    citations.sort((a,b)=>b-a);
+    while(answer<citations.length){
+        //인덱스값이 answer보다 작거나 같으면 answer+1
+        if(citations[answer] <= answer)break;
+        answer++;
+    }
+    return answer;
+}
+
+/*
+0,7
+1,6
+2,4
+3,1 -> 인덱스3에서 값1보다 커졌으므로 3리턴
+4,1
+5,0
+*/
