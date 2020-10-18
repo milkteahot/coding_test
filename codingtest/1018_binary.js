94b04ea1577a3443509b8696fa906a1a0ff660fe
milkteahot <greenegoode11@gmail.com>
2020-10-18 22:55:43 +0900

    Add 1018_binary.js

---
+++ codingtest/1018_binary.js
@@ -0,0 +1,22 @@
+function binarySearch(sortedArray, seekElement) {
+    let startIndex = 0;
+    let endIndex = sortedArray.length - 1;
+
+    while(startIndex <= endIndex) {
+        const middleIndex = startIndex + Math.floor((endIndex - startIndex)/2);
+        if(sortedArray[middleIndex] === seekElement){
+            return middleIndex;
+        }
+        if(sortedArray[middleIndex] < seekElement){
+            startIndex = middleIndex + 1;
+        } else {
+            endIndex = middleIndex - 1;
+        }
+    }
+    return -1;
+}
+
+const nums = [10,40,50,30,60,70,80,90,20];
+const sortedNums = nums.sort();
+
+console.log(binarySearch(sortedNums, 30));
